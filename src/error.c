3385eef58628fde6591072a9a1ca785ab1f3543f
stephdaniel133 <stephdaniel133@hotmail.com>
2020-05-01 13:24:31 +0200

    Retrait d'espaces inutiles

--- src/error.c
+++ src/error.c
@@ -12,7 +12,7 @@ void print_info(global_t *pGlobal, char *format, ...)
 {
     va_list va;
 
-    va_start (va, format);
+    va_start(va, format);
     print_message(pGlobal, GTK_MESSAGE_INFO, format, va);
     va_end(va);
 }
@@ -21,7 +21,7 @@ void print_warning(global_t *pGlobal, char *format, ...)
 {
     va_list va;
 
-    va_start (va, format);
+    va_start(va, format);
     print_message(pGlobal, GTK_MESSAGE_WARNING, format, va);
     va_end(va);
 }
@@ -30,8 +30,8 @@ void print_error(global_t *pGlobal, char *format, ...)
 {
     va_list va;
 
-    va_start (va, format);
-    print_message (pGlobal, GTK_MESSAGE_ERROR, format, va);
+    va_start(va, format);
+    print_message(pGlobal, GTK_MESSAGE_ERROR, format, va);
     va_end(va);
     exit (EXIT_FAILURE);
 }
@@ -45,7 +45,7 @@ static void print_message(global_t *pGlobal, GtkMessageType type, const gchar *f
     printf("%s", message);
     p_dialog = gtk_message_dialog_new (GTK_WINDOW(pGlobal->pMainWindow), GTK_DIALOG_MODAL, type, GTK_BUTTONS_OK, "%s", message);
 
-    gtk_dialog_run (GTK_DIALOG (p_dialog));
+    gtk_dialog_run(GTK_DIALOG (p_dialog));
     gtk_widget_destroy (p_dialog);
 
     g_free (message);
